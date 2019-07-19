3ca73d0b7cd0c6e0006446e9130b451ff09cb060
Tenten Ponce <poncetenten10@gmail.com>
2019-07-19 16:25:03 +0800

    Removed unecessary checking of pattern for integer

--- flexvalidation/src/main/java/com/tcorner/flexvalidation/YearValidation.kt
+++ flexvalidation/src/main/java/com/tcorner/flexvalidation/YearValidation.kt
@@ -21,7 +21,7 @@ class YearValidation : Validation() {
     override fun isValid(o: Any): Boolean {
         return when (o) {
             is String -> o.matches(YEAR_VALIDATION.toRegex()) && o.length == YEAR_LENGTH
-            is Int -> o.toString().matches(YEAR_VALIDATION.toRegex()) && o.toString().length == YEAR_LENGTH
+            is Int -> o.toString().length == YEAR_LENGTH
             else -> false
         }
     }
